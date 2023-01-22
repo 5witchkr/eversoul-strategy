8c4ffa0befbad630e5674d857f5d84aa6d1f4d4c
5witchkr <hob4419@gmail.com>
2023-01-23 03:37:12 +0900

    feat: add inbound-adapter

---
+++ soul-character/src/main/java/com/strategy/adapter/inbound/presentation/SoulCharacterApi.java
@@ -0,0 +1,9 @@
+package com.strategy.adapter.inbound.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/soulcharacter")
+public class SoulCharacterApi {
+}
