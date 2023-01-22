2c1de407b0c869ece11be35676e2a48e3eb3dca7
5witchkr <hob4419@gmail.com>
2023-01-23 04:20:26 +0900

    feat: add inbound-port getTier method

--- soul-character/src/main/java/com/strategy/application/port/inbound/SoulCharacterInboundPort.java
+++ soul-character/src/main/java/com/strategy/application/port/inbound/SoulCharacterInboundPort.java
@@ -1,4 +1,7 @@
 package com.strategy.application.port.inbound;
 
+import com.strategy.application.port.inbound.outputdto.TierDto;
+
 public interface SoulCharacterInboundPort {
+    TierDto getTier(Long tier);
 }
