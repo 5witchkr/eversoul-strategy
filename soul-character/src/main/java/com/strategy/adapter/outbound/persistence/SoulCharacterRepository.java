18f30458a7cee9a035f1d807f268f8cb991eca68
5witchkr <hob4419@gmail.com>
2023-01-23 17:41:37 +0900

    feat: add data jpaRepository

--- soul-character/src/main/java/com/strategy/adapter/outbound/persistence/SoulCharacterRepository.java
+++ soul-character/src/main/java/com/strategy/adapter/outbound/persistence/SoulCharacterRepository.java
@@ -1,4 +1,11 @@
 package com.strategy.adapter.outbound.persistence;
 
-public interface SoulCharacterRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SoulCharacterRepository extends JpaRepository<SoulCharacter, Long> {
+
+    Optional<List<SoulCharacter>> getByTier(String tier);
 }
