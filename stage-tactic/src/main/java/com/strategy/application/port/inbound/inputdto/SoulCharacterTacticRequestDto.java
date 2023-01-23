2ede4d8b2f5b4b0d36ebc9fcc0718f475e911880
5witchkr <hob4419@gmail.com>
2023-01-24 02:22:48 +0900

    feat: SoulCharacter request dto for request tactic dto's vo

---
+++ stage-tactic/src/main/java/com/strategy/application/port/inbound/inputdto/SoulCharacterTacticRequestDto.java
@@ -0,0 +1,9 @@
+package com.strategy.application.port.inbound.inputdto;
+
+import lombok.Getter;
+
+@Getter
+public class SoulCharacterTacticRequestDto {
+    private String name;
+    private String level;
+}
