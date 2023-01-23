6fc1a393510d05f34e1fcec49db3b782152adbbf
5witchkr <hob4419@gmail.com>
2023-01-24 02:27:29 +0900

    feat: add tactic domain entity

---
+++ stage-tactic/src/main/java/com/strategy/adpater/outbound/persistence/entity/Tactic.java
@@ -0,0 +1,43 @@
+package com.strategy.adpater.outbound.persistence.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tactic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String position;
+
+    @Column
+    private String power;
+
+    @Column
+    private String info;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stage_id", referencedColumnName = "id")
+    private Stage stage;
+
+    @OneToMany(mappedBy = "tactic", cascade = CascadeType.ALL)
+    private List<TacticCharacter> tacticCharacters = new ArrayList<>();
+
+    @Builder
+    public Tactic(String position, String power, String info, Stage stage, List<TacticCharacter> tacticCharacters) {
+        this.position = position;
+        this.power = power;
+        this.info = info;
+        this.stage = stage;
+        this.tacticCharacters = tacticCharacters;
+    }
+}
