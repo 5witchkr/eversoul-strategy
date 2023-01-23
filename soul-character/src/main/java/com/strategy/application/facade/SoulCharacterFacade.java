6b1a912a4e28a5ca82a8d16f869ad899c7e10c1a
5witchkr <hob4419@gmail.com>
2023-01-23 17:40:48 +0900

    feat: add SoulCharacterFacade

--- soul-character/src/main/java/com/strategy/application/facade/SoulCharacterFacade.java
+++ soul-character/src/main/java/com/strategy/application/facade/SoulCharacterFacade.java
@@ -1,14 +1,26 @@
 package com.strategy.application.facade;
 
-import com.strategy.application.port.inbound.outputdto.TierDto;
-import com.strategy.application.port.inbound.SoulCharacterInboundPort;
+import com.strategy.application.port.inbound.outputdto.SoulCharacterResponseDto;
+import com.strategy.application.processor.TierProcessor;
+import com.strategy.application.validator.TierValidator;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
-public class SoulCharacterFacade implements SoulCharacterInboundPort {
-    @Override
-    public TierDto getTier(Long tier) {
-        return null;
+public class SoulCharacterFacade {
+
+    private final TierProcessor tierProcessor;
+    private final TierValidator tierValidator;
+
+    public SoulCharacterFacade(TierProcessor tierProcessor, TierValidator tierValidator) {
+        this.tierProcessor = tierProcessor;
+        this.tierValidator = tierValidator;
+    }
+
+    public List<SoulCharacterResponseDto> getSoulByTier(String tier) {
+        tierValidator.checkInputValue(tier);
+        return tierProcessor.getSoulByTier(tier);
     }
 }
