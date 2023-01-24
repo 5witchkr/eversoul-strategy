e1f2bc57ce80daecefcd6facd76358f276d5778c
5witchkr <hob4419@gmail.com>
2023-01-25 05:32:54 +0900

    fix: management class name

--- soul-character/src/main/java/com/strategy/application/facade/SoulManagementFacade.java
+++ soul-character/src/main/java/com/strategy/application/facade/SoulManagementFacade.java
@@ -2,28 +2,28 @@ package com.strategy.application.facade;
 
 import com.strategy.application.port.inbound.inputdto.SoulPutRequestDto;
 import com.strategy.application.port.inbound.inputdto.SoulSaveRequestDto;
-import com.strategy.application.processor.SoulManagementProcessor;
+import com.strategy.application.processor.SoulTierManagementProcessor;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SoulManagementFacade {
 
-    private final SoulManagementProcessor soulManagementProcessor;
+    private final SoulTierManagementProcessor soulTierManagementProcessor;
 
-    public SoulManagementFacade(SoulManagementProcessor soulManagementProcessor) {
-        this.soulManagementProcessor = soulManagementProcessor;
+    public SoulManagementFacade(SoulTierManagementProcessor soulTierManagementProcessor) {
+        this.soulTierManagementProcessor = soulTierManagementProcessor;
     }
 
 
     public void saveSoul(SoulSaveRequestDto soulSaveRequestDto) {
-        soulManagementProcessor.saveSoul(soulSaveRequestDto);
+        soulTierManagementProcessor.saveSoul(soulSaveRequestDto);
     }
 
     public void deleteSoul(Long id) {
-        soulManagementProcessor.deleteSoul(id);
+        soulTierManagementProcessor.deleteSoul(id);
     }
 
     public void putSoul(SoulPutRequestDto soulPutRequestDto) {
-        soulManagementProcessor.putSoul(soulPutRequestDto);
+        soulTierManagementProcessor.putSoul(soulPutRequestDto);
     }
 }
