4d72e1d2b3fb932d5a213fa15b4d401c742d54ef
5witchkr <hob4419@gmail.com>
2023-01-24 19:13:19 +0900

    refactor: String to int and add builder

--- stage-tactic/src/main/java/com/strategy/application/port/inbound/inputdto/SoulCharacterTacticRequestDto.java
+++ stage-tactic/src/main/java/com/strategy/application/port/inbound/inputdto/SoulCharacterTacticRequestDto.java
@@ -1,9 +1,16 @@
 package com.strategy.application.port.inbound.inputdto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class SoulCharacterTacticRequestDto {
     private String name;
-    private String level;
+    private int level;
+
+    @Builder
+    public SoulCharacterTacticRequestDto(String name, int level){
+        this.level = level;
+        this.name = name;
+    }
 }
