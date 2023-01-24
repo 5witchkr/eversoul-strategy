4a21b337f1ea018bd83bb5b6f92e83fccef95da2
5witchkr <hob4419@gmail.com>
2023-01-25 03:03:53 +0900

    feat: add inbound-port-inputDto StageSaveDto

---
+++ stage-tactic/src/main/java/com/strategy/application/port/inbound/inputdto/StageSaveDto.java
@@ -0,0 +1,21 @@
+package com.strategy.application.port.inbound.inputdto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class StageSaveDto {
+
+    private int location;
+
+    private int step;
+
+    private String position;
+
+    private int power;
+
+    private List<String> soulCharacters;
+}
