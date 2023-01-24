f17f7c6628dabf1ee48541bba3a1f7662eea11af
5witchkr <hob4419@gmail.com>
2023-01-24 19:15:47 +0900

    feat: StageTacticFacade add validators

--- stage-tactic/src/main/java/com/strategy/application/facade/StageTacticFacade.java
+++ stage-tactic/src/main/java/com/strategy/application/facade/StageTacticFacade.java
@@ -5,9 +5,7 @@ import com.strategy.application.port.inbound.inputdto.TacticRequestDto;
 import com.strategy.application.port.inbound.outputdto.RecommendTacticResponseDto;
 import com.strategy.application.processor.RecommendProcessor;
 import com.strategy.application.processor.TacticProcessor;
-import com.strategy.application.validator.BanSoulsValidator;
-import com.strategy.application.validator.StageParamValidator;
-import com.strategy.application.validator.TacticRequestValidator;
+import com.strategy.application.validator.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,18 +17,27 @@ public class StageTacticFacade {
     private final TacticProcessor tacticProcessor;
     private final BanSoulsValidator banSoulsValidator;
     private final StageParamValidator stageParamValidator;
-    private final TacticRequestValidator tacticRequestValidator;
+    private final InfoValidator infoValidator;
+    private final LevelValidator levelValidator;
+    private final PositionValidator positionValidator;
+    private final PowerValidator powerValidator;
+    private final SoulNameValidator soulNameValidator;
 
 
-    public StageTacticFacade(RecommendProcessor recommendProcessor, TacticProcessor tacticProcessor, BanSoulsValidator banSoulsValidator, StageParamValidator stageParamValidator, TacticRequestValidator tacticRequestValidator) {
+
+    public StageTacticFacade(RecommendProcessor recommendProcessor, TacticProcessor tacticProcessor, BanSoulsValidator banSoulsValidator, StageParamValidator stageParamValidator, InfoValidator infoValidator, LevelValidator levelValidator, PositionValidator positionValidator, PowerValidator powerValidator, SoulNameValidator soulNameValidator) {
         this.recommendProcessor = recommendProcessor;
         this.tacticProcessor = tacticProcessor;
         this.banSoulsValidator = banSoulsValidator;
         this.stageParamValidator = stageParamValidator;
-        this.tacticRequestValidator = tacticRequestValidator;
+        this.infoValidator = infoValidator;
+        this.levelValidator = levelValidator;
+        this.positionValidator = positionValidator;
+        this.powerValidator = powerValidator;
+        this.soulNameValidator = soulNameValidator;
     }
 
-    public List<RecommendTacticResponseDto> getRecommendWithoutBans(String location, String step, List<String> bans) {
+    public List<RecommendTacticResponseDto> getRecommendWithoutBans(int location, int step, List<String> bans) {
         stageParamValidator.checkLocation(location);
         stageParamValidator.checkStep(step);
         List<String> filteredBans = banSoulsValidator.filterBanSouls(bans);
@@ -38,7 +45,13 @@ public class StageTacticFacade {
     }
 
     public void postTactic(TacticRequestDto tacticRequestDto){
-        tacticRequestValidator.checkTacticRequest(tacticRequestDto);
+        stageParamValidator.checkLocation(tacticRequestDto.getLocation());
+        stageParamValidator.checkStep(tacticRequestDto.getStep());
+        infoValidator.checkInfo(tacticRequestDto.getInfo());
+        positionValidator.checkPosition(tacticRequestDto.getPosition());
+        powerValidator.checkPower(tacticRequestDto.getPower());
+        levelValidator.checkLevelByDtos(tacticRequestDto.getSoulCharacters());
+        soulNameValidator.checkSoulNameByDtos(tacticRequestDto.getSoulCharacters());
         tacticProcessor.postTactic(tacticRequestDto);
     }
 }
