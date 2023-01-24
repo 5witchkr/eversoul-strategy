d11f60427d54fc4e6f386a436cddf8111065e336
5witchkr <hob4419@gmail.com>
2023-01-24 12:35:30 +0900

    feat: add input value validator

--- stage-tactic/src/main/java/com/strategy/application/facade/StageTacticFacade.java
+++ stage-tactic/src/main/java/com/strategy/application/facade/StageTacticFacade.java
@@ -5,6 +5,9 @@ import com.strategy.application.port.inbound.inputdto.TacticRequestDto;
 import com.strategy.application.port.inbound.outputdto.RecommendTacticResponseDto;
 import com.strategy.application.processor.RecommendProcessor;
 import com.strategy.application.processor.TacticProcessor;
+import com.strategy.application.validator.BanSoulsValidator;
+import com.strategy.application.validator.StageParamValidator;
+import com.strategy.application.validator.TacticRequestValidator;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,18 +17,28 @@ public class StageTacticFacade {
 
     private final RecommendProcessor recommendProcessor;
     private final TacticProcessor tacticProcessor;
+    private final BanSoulsValidator banSoulsValidator;
+    private final StageParamValidator stageParamValidator;
+    private final TacticRequestValidator tacticRequestValidator;
 
 
-    public StageTacticFacade(RecommendProcessor recommendProcessor, TacticProcessor tacticProcessor) {
+    public StageTacticFacade(RecommendProcessor recommendProcessor, TacticProcessor tacticProcessor, BanSoulsValidator banSoulsValidator, StageParamValidator stageParamValidator, TacticRequestValidator tacticRequestValidator) {
         this.recommendProcessor = recommendProcessor;
         this.tacticProcessor = tacticProcessor;
+        this.banSoulsValidator = banSoulsValidator;
+        this.stageParamValidator = stageParamValidator;
+        this.tacticRequestValidator = tacticRequestValidator;
     }
 
     public List<RecommendTacticResponseDto> getRecommendWithoutBans(String location, String step, List<String> bans) {
+        stageParamValidator.checkLocation(location);
+        stageParamValidator.checkStep(step);
+        banSoulsValidator.checkBanSouls(bans);
         return recommendProcessor.getRecommendWithoutBans(location ,step ,bans);
     }
 
     public void postTactic(TacticRequestDto tacticRequestDto){
+        tacticRequestValidator.checkTacticRequest(tacticRequestDto);
         tacticProcessor.postTactic(tacticRequestDto);
     }
 }
