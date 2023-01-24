5c284c2397ec130d2b77d4349b8bf721deb93b8d
5witchkr <hob4419@gmail.com>
2023-01-24 19:17:38 +0900

    feat: init infoValidator

---
+++ stage-tactic/src/main/java/com/strategy/application/validator/InfoValidator.java
@@ -0,0 +1,10 @@
+package com.strategy.application.validator;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InfoValidator {
+
+    public void checkInfo(String info){}
+}
