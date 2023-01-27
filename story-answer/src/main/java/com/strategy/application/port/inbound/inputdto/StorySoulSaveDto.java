d93e1cfa34cf8d87058400dfc7abd59ac22c86ce
5witchkr <hob4419@gmail.com>
2023-01-27 20:37:34 +0900

    feat: inbound-inputDto StorySoul

---
+++ story-answer/src/main/java/com/strategy/application/port/inbound/inputdto/StorySoulSaveDto.java
@@ -0,0 +1,15 @@
+package com.strategy.application.port.inbound.inputdto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StorySoulSaveDto {
+
+    private String name;
+
+    private String type;
+
+    private String tier;
+}
