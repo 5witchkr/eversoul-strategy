dac1858d1ffa5bf9ae8157ecf981e42e50de6d53
5witchkr <hob4419@gmail.com>
2023-01-27 22:48:50 +0900

    fix: duplicate class name

--- stage-tactic/src/main/java/com/strategy/application/port/inbound/SaveTacticSoulInboundPort.java
+++ stage-tactic/src/main/java/com/strategy/application/port/inbound/SaveTacticSoulInboundPort.java
@@ -2,6 +2,6 @@ package com.strategy.application.port.inbound;
 
 import com.strategy.application.port.inbound.inputdto.SoulSaveDto;
 
-public interface SaveSoulInboundPort {
+public interface SaveTacticSoulInboundPort {
     void saveSoul(SoulSaveDto soulSaveDto);
 }
