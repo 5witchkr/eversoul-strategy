fcb47b70db31c40abfde753a84ac23358df0ba91
5witchkr <hob4419@gmail.com>
2023-01-27 20:50:16 +0900

    fix: port Facade SoulCharacter

--- soul-character/src/main/java/com/strategy/application/facade/SoulCharacterFacade.java
+++ soul-character/src/main/java/com/strategy/application/facade/SoulCharacterFacade.java
@@ -1,7 +1,7 @@
 package com.strategy.application.facade;
 
+import com.strategy.application.port.inbound.GetTierInboundPort;
 import com.strategy.application.port.inbound.outputdto.SoulCharacterResponseDto;
-import com.strategy.application.processor.TierProcessor;
 import com.strategy.application.validator.TierValidator;
 import org.springframework.stereotype.Service;
 
@@ -11,16 +11,16 @@ import java.util.List;
 @Service
 public class SoulCharacterFacade {
 
-    private final TierProcessor tierProcessor;
+    private final GetTierInboundPort getTierInboundPort;
     private final TierValidator tierValidator;
 
-    public SoulCharacterFacade(TierProcessor tierProcessor, TierValidator tierValidator) {
-        this.tierProcessor = tierProcessor;
+    public SoulCharacterFacade(GetTierInboundPort getTierInboundPort, TierValidator tierValidator) {
+        this.getTierInboundPort = getTierInboundPort;
         this.tierValidator = tierValidator;
     }
 
     public List<SoulCharacterResponseDto> getSoulByTier(String tier) {
         tierValidator.checkInputValue(tier);
-        return tierProcessor.getSoulByTier(tier);
+        return getTierInboundPort.getSoulByTier(tier);
     }
 }
