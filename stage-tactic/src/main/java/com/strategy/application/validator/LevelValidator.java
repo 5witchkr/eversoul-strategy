35dbfbb142a832b5c7419f4102cf28834767168f
5witchkr <hob4419@gmail.com>
2023-01-28 23:50:11 +0900

    fix: maxlevel 1000 -> 2000

--- stage-tactic/src/main/java/com/strategy/application/validator/LevelValidator.java
+++ stage-tactic/src/main/java/com/strategy/application/validator/LevelValidator.java
@@ -13,7 +13,7 @@ public class LevelValidator {
 
     public void checkLevelByDtos(List<SoulCharacterTacticRequestDto> soulCharacters) {
         long failValueCount = soulCharacters.stream()
-                .filter(value -> value.getLevel() < 1 || value.getLevel() > 1000)
+                .filter(value -> 1 > value.getLevel() || value.getLevel() > 2000)
                 .count();
         if (failValueCount > 0) throw new IllegalArgumentException("유효하지 않은 레벨");
     }
