c5f4c2970ba126d1251f021c4a96e836572c0378
5witchkr <hob4419@gmail.com>
2023-01-28 23:53:25 +0900

    feat: EpisodeEnding Validator

--- story-answer/src/main/java/com/strategy/application/validator/StoryEpisodeValidator.java
+++ story-answer/src/main/java/com/strategy/application/validator/StoryEpisodeValidator.java
@@ -1,8 +1,18 @@
 package com.strategy.application.validator;
 
 
+import com.strategy.enummodel.StoryEpisodeEnum;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StoryEpisodeValidator {
+
+
+    public void checkEpisode(int value) {
+        if (StoryEpisodeEnum.MIN_EPISODE.getValue() <= value
+                && value <= StoryEpisodeEnum.MAX_EPISODE.getValue()){
+            return;
+        }
+        throw new IllegalArgumentException("유효하지 않은 에피소드 값");
+    }
 }
