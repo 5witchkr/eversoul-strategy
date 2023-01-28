f559a9ae89d1bb576700bde6c8231bd8ed378f25
5witchkr <hob4419@gmail.com>
2023-01-28 20:20:45 +0900

    feat: add test sql

--- stage-tactic/src/main/java/com/strategy/application/validator/TacticSoulIdValidator.java
+++ stage-tactic/src/main/java/com/strategy/application/validator/TacticSoulIdValidator.java
@@ -3,7 +3,6 @@ package com.strategy.application.validator;
 
 import com.strategy.application.port.inbound.inputdto.SoulCharacterTacticRequestDto;
 import com.strategy.application.port.inbound.inputdto.SoulCharacterTacticValidReqDto;
-import com.strategy.soulconstant.SoulNameConstants;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
