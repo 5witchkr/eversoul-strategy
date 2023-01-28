6884e106328fa468869887b4c4492d5698fe3d0e
5witchkr <hob4419@gmail.com>
2023-01-28 20:09:19 +0900

    feat: add TacticCharacter outbound-port saveAll

--- stage-tactic/src/main/java/com/strategy/application/port/outbound/TacticCharacterOutboundPort.java
+++ stage-tactic/src/main/java/com/strategy/application/port/outbound/TacticCharacterOutboundPort.java
@@ -2,7 +2,11 @@ package com.strategy.application.port.outbound;
 
 import com.strategy.adpater.outbound.persistence.entity.TacticCharacter;
 
+import java.util.List;
+
 public interface TacticCharacterOutboundPort {
 
     TacticCharacter save(TacticCharacter tacticCharacter);
+
+    List<TacticCharacter> saveAll(List<TacticCharacter> tacticCharacters);
 }
