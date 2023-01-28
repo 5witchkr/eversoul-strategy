accb9949048ffc87c593478bdf32534eec1e04f3
5witchkr <hob4419@gmail.com>
2023-01-28 20:05:46 +0900

    refactor: query optimization

--- stage-tactic/src/main/java/com/strategy/application/port/inbound/PostTacticInboundPort.java
+++ stage-tactic/src/main/java/com/strategy/application/port/inbound/PostTacticInboundPort.java
@@ -1,8 +1,12 @@
 package com.strategy.application.port.inbound;
 
+import com.strategy.application.port.inbound.inputdto.SoulCharacterTacticValidReqDto;
 import com.strategy.application.port.inbound.inputdto.TacticRequestDto;
+
+import java.util.List;
 
 public interface PostTacticInboundPort {
 
-    void postTactic(TacticRequestDto tacticRequestDto);
+    void postTactic(TacticRequestDto tacticRequestDto,
+                    List<SoulCharacterTacticValidReqDto> soulCharacterTacticValidReqDtos);
 }
