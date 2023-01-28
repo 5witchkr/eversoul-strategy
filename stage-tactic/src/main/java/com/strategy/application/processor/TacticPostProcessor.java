8bec9fb8cbeb53e7521093770d7c0a48a51d98ec
5witchkr <hob4419@gmail.com>
2023-01-28 20:11:33 +0900

    refactor: tactic Post logic

--- stage-tactic/src/main/java/com/strategy/application/processor/TacticPostProcessor.java
+++ stage-tactic/src/main/java/com/strategy/application/processor/TacticPostProcessor.java
@@ -5,26 +5,31 @@ import com.strategy.adpater.outbound.persistence.entity.Tactic;
 import com.strategy.adpater.outbound.persistence.entity.TacticCharacter;
 import com.strategy.adpater.outbound.persistence.entity.TacticSoulcharacter;
 import com.strategy.application.port.inbound.PostTacticInboundPort;
-import com.strategy.application.port.inbound.inputdto.SoulCharacterTacticRequestDto;
+import com.strategy.application.port.inbound.inputdto.SoulCharacterTacticValidReqDto;
 import com.strategy.application.port.inbound.inputdto.TacticRequestDto;
 import com.strategy.application.port.outbound.StageOutboundPort;
 import com.strategy.application.port.outbound.TacticCharacterOutboundPort;
 import com.strategy.application.port.outbound.TacticOutboundPort;
 import com.strategy.application.port.outbound.TacticSoulcharacterOutboundPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
-public class TacticProcessor implements PostTacticInboundPort {
+public class TacticPostProcessor implements PostTacticInboundPort {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
     private final TacticOutboundPort tacticOutboundPort;
     private final StageOutboundPort stageOutboundPort;
     private final TacticSoulcharacterOutboundPort tacticSoulcharacterOutboundPort;
     private final TacticCharacterOutboundPort tacticCharacterOutboundPort;
 
-    public TacticProcessor(TacticOutboundPort tacticOutboundPort, StageOutboundPort stageOutboundPort, TacticSoulcharacterOutboundPort tacticSoulcharacterOutboundPort, TacticCharacterOutboundPort tacticCharacterOutboundPort) {
+
+    public TacticPostProcessor(TacticOutboundPort tacticOutboundPort, StageOutboundPort stageOutboundPort, TacticSoulcharacterOutboundPort tacticSoulcharacterOutboundPort, TacticCharacterOutboundPort tacticCharacterOutboundPort) {
         this.tacticOutboundPort = tacticOutboundPort;
         this.stageOutboundPort = stageOutboundPort;
         this.tacticSoulcharacterOutboundPort = tacticSoulcharacterOutboundPort;
@@ -33,7 +38,7 @@ public class TacticProcessor implements PostTacticInboundPort {
 
     @Override
     @Transactional
-    public void postTactic(TacticRequestDto tacticRequestDto) {
+    public void postTactic(TacticRequestDto tacticRequestDto, List<SoulCharacterTacticValidReqDto> soulCharacterTacticValidReqDtos) {
         Tactic tactic = tacticOutboundPort.save(Tactic.builder()
                 .stage(getByLocationAndStep(tacticRequestDto.getLocation(), tacticRequestDto.getStep()))
                 .info(tacticRequestDto.getInfo())
@@ -41,20 +46,35 @@ public class TacticProcessor implements PostTacticInboundPort {
                 .power(tacticRequestDto.getPower())
                 .build());
 
-        inputTacticCharacter(tacticRequestDto.getSoulCharacters(), tactic);
+        inputTacticCharacter(soulCharacterTacticValidReqDtos, tactic);
     }
 
-    private void inputTacticCharacter(List<SoulCharacterTacticRequestDto> soulCharacterTacticRequestDtos, Tactic tactic) {
-        soulCharacterTacticRequestDtos.forEach(value -> tacticCharacterOutboundPort.save(TacticCharacter.builder()
+
+    private void inputTacticCharacter(List<SoulCharacterTacticValidReqDto> soulCharacterTacticValidReqDtos, Tactic tactic) {
+        List<TacticCharacter> tacticCharacters =
+                soulCharacterTacticValidReqDtos.stream()
+                        .map(value -> buildTacticCharacter(tactic,value))
+                        .collect(Collectors.toList());
+        try {
+            tacticCharacterOutboundPort.saveAll(tacticCharacters);
+        } catch (RuntimeException runtimeException){
+            log.warn("fail saveAll tacticCharacters");
+            throw new IllegalArgumentException("유효하지 않은 정보");
+        }
+    }
+
+
+    private TacticCharacter buildTacticCharacter(Tactic tactic, SoulCharacterTacticValidReqDto value) {
+        return TacticCharacter.builder()
                 .tactic(tactic)
                 .level(value.getLevel())
-                .tacticSoulcharacter(getTacticSoulcharacter(value))
-                .build()));
+                .tacticSoulcharacter(getTacticSoulcharacter(value.getSoulId()))
+                .build();
     }
 
-    private TacticSoulcharacter getTacticSoulcharacter(SoulCharacterTacticRequestDto value) {
-        return tacticSoulcharacterOutboundPort.getByName(value.getName())
-                .orElseThrow(() -> new RuntimeException("유효하지 않은 정령"));
+
+    private TacticSoulcharacter getTacticSoulcharacter(Long id) {
+        return tacticSoulcharacterOutboundPort.getByReferenceId(id);
     }
 
     private Stage getByLocationAndStep(int location, int step) {
