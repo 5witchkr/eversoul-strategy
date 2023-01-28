12456aeb80f7db654c2a93a0817157f4527359b8
5witchkr <hob4419@gmail.com>
2023-01-28 20:15:30 +0900

    init: story module tests

---
+++ story-answer/src/test/java/com/strategy/StoryAnswerTestsConfig.java
@@ -0,0 +1,8 @@
+package com.strategy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryAnswerTestsConfig {
+    public void contextLoads() {}
+}
