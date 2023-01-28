eaa50273356d6bdebc799991a235696d0292839f
5witchkr <hob4419@gmail.com>
2023-01-28 20:13:25 +0900

    feat: outbound-port TacticSoulCharacter method

--- stage-tactic/src/main/java/com/strategy/application/port/outbound/TacticSoulcharacterOutboundPort.java
+++ stage-tactic/src/main/java/com/strategy/application/port/outbound/TacticSoulcharacterOutboundPort.java
@@ -14,4 +14,6 @@ public interface TacticSoulcharacterOutboundPort {
     Optional<TacticSoulcharacter> findById(Long id);
 
     boolean existsByName(String name);
+
+    TacticSoulcharacter getByReferenceId(Long id);
 }
