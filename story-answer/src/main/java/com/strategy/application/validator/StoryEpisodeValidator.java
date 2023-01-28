21d54a6e594d14e34f4c6635b18da13b36d17dba
5witchkr <hob4419@gmail.com>
2023-01-28 20:16:34 +0900

    init: story module tests

---
+++ story-answer/src/main/java/com/strategy/application/validator/StoryEpisodeValidator.java
@@ -0,0 +1,8 @@
+package com.strategy.application.validator;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StoryEpisodeValidator {
+}
---
+++ story-answer/src/test/java/com/strategy/validoator/StoryEndingValidator.java
@@ -0,0 +1,4 @@
+package com.strategy.validoator;
+
+public class StoryEndingValidator {
+}
