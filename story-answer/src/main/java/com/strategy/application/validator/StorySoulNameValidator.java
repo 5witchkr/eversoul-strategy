3c943f5aa23f890c476ea6b062c3d00e6447b091
5witchkr <hob4419@gmail.com>
2023-01-28 23:54:07 +0900

    feat: StorySoulNameValidator

--- story-answer/src/main/java/com/strategy/application/validator/StorySoulNameValidator.java
+++ story-answer/src/main/java/com/strategy/application/validator/StorySoulNameValidator.java
@@ -1,8 +1,16 @@
 package com.strategy.application.validator;
 
 
+import com.strategy.constantmodel.SoulNameConstants;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class StorySoulNameValidator {
+    public void checkSoulName(String value) {
+        if (!SoulNameConstants.allSoulNames.contains(value)){
+            throw new IllegalArgumentException("유효하지 않은 정령이름");
+        }
+    }
 }
