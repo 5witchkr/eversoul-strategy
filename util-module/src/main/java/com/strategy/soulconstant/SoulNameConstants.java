b457b4ee8b8c9ebeb7cd858fbb024a63b90d1126
5witchkr <hob4419@gmail.com>
2023-01-28 20:17:24 +0900

    feat: add const final class for allSoulList

---
+++ util-module/src/main/java/com/strategy/soulconstant/SoulNameConstants.java
@@ -0,0 +1,18 @@
+package com.strategy.soulconstant;
+
+import java.util.List;
+
+public final class SoulNameConstants {
+
+    public static final List<String> allSoulNames =
+            List.of("아드리안", "캐서린", "탈리아", "재클린",
+                    "페트라", "비올레트", "지호", "메피",
+                    "비비안", "린지", "나이아", "클레르",
+                    "클로이", "도라", "미리암", "프림",
+                    "셰리", "미카", "시하", "레베카",
+                    "클라라", "하루", "플린", "니니",
+                    "에루샤", "아야메", "순이", "아이라",
+                    "르네", "니콜", "제이드");
+
+
+}
