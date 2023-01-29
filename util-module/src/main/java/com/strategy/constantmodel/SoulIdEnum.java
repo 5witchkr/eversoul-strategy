fba86f6a4fe238c5e9d5af46d8af36224de8050b
5witchkr <hob4419@gmail.com>
2023-01-29 12:26:22 +0900

    feat: add SoulIdEnum

---
+++ util-module/src/main/java/com/strategy/constantmodel/SoulIdEnum.java
@@ -0,0 +1,16 @@
+package com.strategy.constantmodel;
+
+public enum SoulIdEnum {
+    SOUL_ID_START(1L),
+    SOUL_ID_END(31L);
+
+    private final Long value;
+
+    SoulIdEnum(Long value) {
+        this.value = value;
+    }
+
+    public Long getValue(){
+        return value;
+    }
+}
