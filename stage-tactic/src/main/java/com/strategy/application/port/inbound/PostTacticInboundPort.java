4fee2d2a75b16baab88e970303ee7bbbca7d352c
5witchkr <hob4419@gmail.com>
2023-01-29 11:42:38 +0900

    refactor: inbound-inputDto change

--- stage-tactic/src/main/java/com/strategy/application/port/inbound/PostTacticInboundPort.java
+++ stage-tactic/src/main/java/com/strategy/application/port/inbound/PostTacticInboundPort.java
@@ -1,12 +1,9 @@
 package com.strategy.application.port.inbound;
 
-import com.strategy.application.port.inbound.inputdto.SoulCharacterTacticValidReqDto;
 import com.strategy.application.port.inbound.inputdto.TacticRequestDto;
 
-import java.util.List;
 
 public interface PostTacticInboundPort {
 
-    void postTactic(TacticRequestDto tacticRequestDto,
-                    List<SoulCharacterTacticValidReqDto> soulCharacterTacticValidReqDtos);
+    void postTactic(TacticRequestDto tacticRequestDtos);
 }
