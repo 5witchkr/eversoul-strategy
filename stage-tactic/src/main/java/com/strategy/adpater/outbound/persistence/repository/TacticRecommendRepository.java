6effead390176b807a4030c8cf0a4618669fb58f
5witchkr <hob4419@gmail.com>
2023-01-30 12:30:06 +0900

    remove: recommend Table Entity JpaRepository

--- stage-tactic/src/main/java/com/strategy/adpater/outbound/persistence/repository/TacticRecommendRepository.java
+++
@@ -1,7 +0,0 @@
-package com.strategy.adpater.outbound.persistence.repository;
-
-import com.strategy.adpater.outbound.persistence.entity.TacticRecommend;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TacticRecommendRepository extends JpaRepository<TacticRecommend, Long> {
-}
