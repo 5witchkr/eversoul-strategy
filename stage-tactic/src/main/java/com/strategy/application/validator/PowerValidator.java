70d72648d9f4012d15a47b7164e79e23cb76afbb
5witchkr <hob4419@gmail.com>
2023-01-30 11:11:11 +0900

    fix:  default power change 1 -> 0

--- stage-tactic/src/main/java/com/strategy/application/validator/PowerValidator.java
+++ stage-tactic/src/main/java/com/strategy/application/validator/PowerValidator.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class PowerValidator {
 
     public void checkPower(int power){
-        if (0 < power && power < 99999999 ){
+        if (0 <= power && power <= 99999999 ){
             return;
         }
         throw new IllegalArgumentException("유효하지 않은 전투력");
