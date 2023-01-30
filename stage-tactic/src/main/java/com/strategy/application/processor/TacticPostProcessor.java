2315cd25502ba14442eb7ee569c4751fe9eafead
5witchkr <hob4419@gmail.com>
2023-01-31 01:54:15 +0900

    fix: spring annotation

--- stage-tactic/src/main/java/com/strategy/application/processor/TacticPostProcessor.java
+++ stage-tactic/src/main/java/com/strategy/application/processor/TacticPostProcessor.java
@@ -14,8 +14,8 @@ import com.strategy.application.port.outbound.TacticSoulcharacterOutboundPort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
