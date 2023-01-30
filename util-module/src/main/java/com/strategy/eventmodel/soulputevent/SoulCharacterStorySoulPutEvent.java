d219e30c1727ff32a5f048a5410ae3bb4ef3b03a
5witchkr <hob4419@gmail.com>
2023-01-31 01:58:05 +0900

    feat: event model storySoulPut

---
+++ util-module/src/main/java/com/strategy/eventmodel/soulputevent/SoulCharacterStorySoulPutEvent.java
@@ -0,0 +1,32 @@
+package com.strategy.eventmodel.soulputevent;
+
+public class SoulCharacterStorySoulPutEvent {
+    private final Long id;
+    private final String name;
+
+    private final String type;
+
+    private final String tier;
+
+    public SoulCharacterStorySoulPutEvent(Long id, String name, String type, String tier) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.tier = tier;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTier() {
+        return tier;
+    }
+}
