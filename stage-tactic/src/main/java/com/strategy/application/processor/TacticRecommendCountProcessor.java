34ae7a7e67ab187d5bd4d281349b24ca4e572f7d
5witchkr <hob4419@gmail.com>
2023-01-30 21:43:06 +0900

    refactor: extract method checkExistIp

--- stage-tactic/src/main/java/com/strategy/application/processor/TacticRecommendCountProcessor.java
+++ stage-tactic/src/main/java/com/strategy/application/processor/TacticRecommendCountProcessor.java
@@ -3,49 +3,29 @@ package com.strategy.application.processor;
 import com.strategy.adpater.outbound.persistence.entity.Tactic;
 import com.strategy.application.port.inbound.portrecommend.TacticRecommendInboundPort;
 import com.strategy.application.port.outbound.TacticOutboundPort;
-import com.strategy.application.port.outbound.RedisTacticRecommendOutboundPort;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.time.LocalDate;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
 
 @Component
 public class TacticRecommendCountProcessor implements TacticRecommendInboundPort {
 
     private final TacticOutboundPort tacticOutboundPort;
-    private final RedisTacticRecommendOutboundPort redisTacticRecommendOutboundPort;
 
 
-    public TacticRecommendCountProcessor(TacticOutboundPort tacticOutboundPort,
-                                         RedisTacticRecommendOutboundPort redisTacticRecommendOutboundPort1) {
+    public TacticRecommendCountProcessor(TacticOutboundPort tacticOutboundPort) {
         this.tacticOutboundPort = tacticOutboundPort;
-        this.redisTacticRecommendOutboundPort = redisTacticRecommendOutboundPort1;
     }
 
     @Override
     @Transactional
-    public void addRecommend(Long tacticId, String userIp) {
+    public void addRecommend(Long tacticId) {
         try {
-            if (!checkExistIpAtTacticId(String.valueOf(tacticId), userIp, LocalDate.now().toString())) {
-                Tactic tactic = tacticOutboundPort.getReferenceById(tacticId);
-                tactic.setRecommendCount(tactic.getRecommendCount() + 1);
-            }
+            Tactic tactic = tacticOutboundPort.getReferenceById(tacticId);
+            tactic.setRecommendCount(tactic.getRecommendCount() + 1);
+
         } catch (RuntimeException runtimeException) {
             throw new IllegalArgumentException("존재하지 않는 게시글");
         }
-    }
-
-    private Boolean checkExistIpAtTacticId(String tacticId, String userIp, String date){
-        Set<String> recommendedIps = redisTacticRecommendOutboundPort.getRecommendedIps("tactic:"+tacticId+":ips:"+date);
-        if (!recommendedIps.contains(userIp)) {
-            redisTacticRecommendOutboundPort.addOpsForSet("tactic:"+tacticId+":ips:"+date, userIp);
-            redisTacticRecommendOutboundPort.setExpire("tactic:"+tacticId+":ips:"+date, 1, TimeUnit.DAYS);
-            return false;
-        }
-        redisTacticRecommendOutboundPort.setExpire("tactic:"+tacticId+":ips:"+date, 1, TimeUnit.DAYS);
-        return true;
     }
 }
