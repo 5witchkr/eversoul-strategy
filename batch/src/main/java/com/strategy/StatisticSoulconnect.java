65aa6df17d06e58058c65dedb6700065cf2d9ba2
5witchkr <hob4419@gmail.com>
2023-02-01 04:23:57 +0900

    feat: init statisticSoulConnect entity

---
+++ batch/src/main/java/com/strategy/StatisticSoulconnect.java
@@ -0,0 +1,20 @@
+package com.strategy;
+
+import javax.persistence.*;
+
+
+@Entity
+public class StatisticSoulconnect {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long lastBatch;
+
+    @Column //ex 14_24 -> 14와 같이한 24
+    private String connectSouls;
+
+    @Column
+    private int connectCount;
+}
