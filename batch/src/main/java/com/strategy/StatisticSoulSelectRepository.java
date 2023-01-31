3efa42967b245fc9a6d9d2ad4232006671faf521
5witchkr <hob4419@gmail.com>
2023-02-01 04:24:31 +0900

    feat: init statisticSoulSelect repository

---
+++ batch/src/main/java/com/strategy/StatisticSoulSelectRepository.java
@@ -0,0 +1,8 @@
+package com.strategy;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface StatisticSoulSelectRepository extends JpaRepository<StatisticSoulselect, Long> {
+
+}
