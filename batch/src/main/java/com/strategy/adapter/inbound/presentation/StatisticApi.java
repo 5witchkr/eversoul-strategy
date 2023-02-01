b10c0b81dff73aaf8169e413fef8022c368213dd
5witchkr <hob4419@gmail.com>
2023-02-02 00:08:54 +0900

    feat: presentation statisticApi getSelect, getSelectTop, getPosition stats

--- batch/src/main/java/com/strategy/adapter/inbound/presentation/StatisticApi.java
+++ batch/src/main/java/com/strategy/adapter/inbound/presentation/StatisticApi.java
@@ -1,6 +1,37 @@
 package com.strategy.adapter.inbound.presentation;
 
+
+import com.strategy.application.facade.StatisticFacade;
+import com.strategy.application.port.inbound.outputdto.PositionStatResponseDto;
+import com.strategy.application.port.inbound.outputdto.SoulSelectResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/stat")
 public class StatisticApi {
 
-    //통계 정보를 가져오는 layer
+    private final StatisticFacade statisticFacade;
+
+    public StatisticApi(StatisticFacade statisticFacade) {
+        this.statisticFacade = statisticFacade;
+    }
+
+    @GetMapping("/soulselect")
+    public ResponseEntity<List<SoulSelectResponseDto>> getTopSoulSelect(@RequestParam int argNumber) {
+        return ResponseEntity.ok(statisticFacade.getTopSoulSelect(argNumber));
+    }
+
+    @GetMapping("/soulselect/{soulId}")
+    public ResponseEntity<SoulSelectResponseDto> getOneSoulSelect(@PathVariable Long soulId) {
+        return ResponseEntity.ok(statisticFacade.getOneSoulSelect(soulId));
+    }
+
+    @GetMapping("/position")
+    public ResponseEntity<List<PositionStatResponseDto>> getPositionStat() {
+        return ResponseEntity.ok(statisticFacade.getPositionStat());
+    }
 }
