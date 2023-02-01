b242f0a19f39bb35a6aa2f6e14de40d036feb680
5witchkr <hob4419@gmail.com>
2023-02-02 00:10:25 +0900

    feat: outbound-port stat position findAll method

--- batch/src/main/java/com/strategy/application/port/outbound/StatisticPositionOutboundPort.java
+++ batch/src/main/java/com/strategy/application/port/outbound/StatisticPositionOutboundPort.java
@@ -2,10 +2,14 @@ package com.strategy.application.port.outbound;
 
 import com.strategy.adapter.outbound.persistence.StatisticPosition;
 
+import java.util.List;
+
 public interface StatisticPositionOutboundPort {
 
 
     StatisticPosition getReferenceById(Long id);
 
     void save(StatisticPosition statisticPosition);
+
+    List<StatisticPosition> findAll();
 }
