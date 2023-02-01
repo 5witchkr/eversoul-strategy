3bd6c04b9377e9dff4c762cb2e37cefdcdf61806
5witchkr <hob4419@gmail.com>
2023-02-01 18:19:41 +0900

    feat: batch presentation layer on-demand api

---
+++ batch/src/main/java/com/strategy/adapter/inbound/presentation/BatchOnDemandApi.java
@@ -0,0 +1,43 @@
+package com.strategy.adapter.inbound.presentation;
+
+
+import com.strategy.application.facade.BatchFacade;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping("/api/batch-admin")
+public class BatchOnDemandApi {
+
+    @Value("${devValue}")
+    private String value;
+
+    private final BatchFacade batchFacade;
+
+
+    public BatchOnDemandApi(BatchFacade batchFacade) {
+        this.batchFacade = batchFacade;
+    }
+
+    @PostMapping("/soulselect")
+    public String postSoulSelect(@RequestParam String devValue) {
+
+        if (!devValue.equals(value)) return "X";
+
+        batchFacade.soulSelectBatch();
+
+        return "O";
+    }
+
+    @PostMapping("/position")
+    public String postPosition(@RequestParam String devValue) {
+
+        if (!devValue.equals(value)) return "X";
+
+        batchFacade.positionBatch();
+
+        return "O";
+    }
+}
