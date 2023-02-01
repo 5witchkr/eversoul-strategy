2ad2d82bd8dcaa08c644a29fcc2b8c72c32cecdf
5witchkr <hob4419@gmail.com>
2023-02-01 21:13:59 +0900

    feat: processor stat position

---
+++ batch/src/main/java/com/strategy/application/processor/StatisticPositionProcessor.java
@@ -0,0 +1,40 @@
+package com.strategy.application.processor;
+
+
+import com.strategy.adapter.outbound.persistence.StatisticPosition;
+import com.strategy.application.port.inbound.StatisticPositionInboundPort;
+import com.strategy.application.port.outbound.StatisticPositionOutboundPort;
+import com.strategy.application.port.outbound.TacticOutboundPort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StatisticPositionProcessor implements StatisticPositionInboundPort {
+
+    private final TacticOutboundPort tacticOutboundPort;
+    private final StatisticPositionOutboundPort statisticPositionOutboundPort;
+
+    public StatisticPositionProcessor(TacticOutboundPort tacticOutboundPort, StatisticPositionOutboundPort statisticPositionOutboundPort) {
+        this.tacticOutboundPort = tacticOutboundPort;
+        this.statisticPositionOutboundPort = statisticPositionOutboundPort;
+    }
+
+
+    @Override
+    public Long getAddedCount() {
+        Long currentCount = tacticOutboundPort.countAll();
+        StatisticPosition statisticPosition = statisticPositionOutboundPort.getReferenceById(1L);
+        Long lastBatchId = statisticPosition.getLastBatch();
+        if (lastBatchId == null) {
+            lastBatchId = 0L;
+        }
+        return currentCount - lastBatchId;
+    }
+
+    @Override
+    public void saveLastCount() {
+        Long currentCount = tacticOutboundPort.countAll();
+        StatisticPosition statisticPosition = statisticPositionOutboundPort.getReferenceById(1L);
+        statisticPosition.setLastBatch(currentCount);
+        statisticPositionOutboundPort.save(statisticPosition);
+    }
+}
