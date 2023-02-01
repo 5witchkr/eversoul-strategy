7e44c9666ade3af5cef340a49e3a85e94c459463
5witchkr <hob4419@gmail.com>
2023-02-01 23:22:38 +0900

    feat: soulconnectBatchData JPA repository

---
+++ batch/src/main/java/com/strategy/adapter/outbound/persistence/SoulconnectBatchdataRepository.java
@@ -0,0 +1,6 @@
+package com.strategy.adapter.outbound.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoulconnectBatchdataRepository extends JpaRepository<SoulconnectBatchdata, Long> {
+}
