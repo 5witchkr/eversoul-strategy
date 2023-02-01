1bcbd85104ac08548b1a019a3aef5d9264c43e5b
5witchkr <hob4419@gmail.com>
2023-02-01 21:10:24 +0900

    feat: port-facade positionBatch

--- batch/src/main/java/com/strategy/application/facade/BatchFacade.java
+++ batch/src/main/java/com/strategy/application/facade/BatchFacade.java
@@ -1,6 +1,8 @@
 package com.strategy.application.facade;
 
+import com.strategy.application.port.inbound.PositionBatchInboundPort;
 import com.strategy.application.port.inbound.SoulSelectBatchInboundPort;
+import com.strategy.application.port.inbound.StatisticPositionInboundPort;
 import com.strategy.application.port.inbound.StatisticSoulSelectInboundPort;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +12,14 @@ public class BatchFacade {
 
     private final StatisticSoulSelectInboundPort statisticSoulSelectInboundPort;
     private final SoulSelectBatchInboundPort soulSelectBatchInboundPort;
+    private final StatisticPositionInboundPort statisticPositionInboundPort;
+    private final PositionBatchInboundPort positionBatchInboundPort;
 
-    public BatchFacade(StatisticSoulSelectInboundPort statisticSoulSelectInboundPort, SoulSelectBatchInboundPort soulSelectBatchInboundPort) {
+    public BatchFacade(StatisticSoulSelectInboundPort statisticSoulSelectInboundPort, SoulSelectBatchInboundPort soulSelectBatchInboundPort, StatisticPositionInboundPort statisticPositionInboundPort, PositionBatchInboundPort positionBatchInboundPort) {
         this.statisticSoulSelectInboundPort = statisticSoulSelectInboundPort;
         this.soulSelectBatchInboundPort = soulSelectBatchInboundPort;
+        this.statisticPositionInboundPort = statisticPositionInboundPort;
+        this.positionBatchInboundPort = positionBatchInboundPort;
     }
 
 
@@ -25,6 +31,8 @@ public class BatchFacade {
     }
 
     public void positionBatch() {
-
+        Long addedCount = statisticPositionInboundPort.getAddedCount();
+        positionBatchInboundPort.addData(addedCount);
+        statisticPositionInboundPort.saveLastCount();
     }
 }
