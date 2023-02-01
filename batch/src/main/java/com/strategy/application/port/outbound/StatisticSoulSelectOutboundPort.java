0c399e2cd56223ef0e30937643a2bdd7ebff91e8
5witchkr <hob4419@gmail.com>
2023-02-02 00:11:22 +0900

    fix: outbound-port selectStat findall

--- batch/src/main/java/com/strategy/application/port/outbound/StatisticSoulSelectOutboundPort.java
+++ batch/src/main/java/com/strategy/application/port/outbound/StatisticSoulSelectOutboundPort.java
@@ -2,9 +2,13 @@ package com.strategy.application.port.outbound;
 
 import com.strategy.adapter.outbound.persistence.StatisticSoulselect;
 
+import java.util.List;
+
 public interface StatisticSoulSelectOutboundPort {
 
     StatisticSoulselect getReferenceById(Long id);
 
     void save(StatisticSoulselect statisticSoulselect);
+
+    List<StatisticSoulselect> findAll();
 }
