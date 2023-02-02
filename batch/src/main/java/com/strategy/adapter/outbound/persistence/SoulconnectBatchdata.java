411841e0eaf9cc038fd49697d707f985bb5d764a
5witchkr <hob4419@gmail.com>
2023-02-02 21:35:18 +0900

    refactor: connect batchdata entity

--- batch/src/main/java/com/strategy/adapter/outbound/persistence/SoulconnectBatchdata.java
+++ batch/src/main/java/com/strategy/adapter/outbound/persistence/SoulconnectBatchdata.java
@@ -16,16 +16,48 @@ public class SoulconnectBatchdata {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;    //tacticId
 
     @Column
-    private String tacticAndSoul; //ex tactic 1, soul 4  -> 1-4
+    private Long soul1;
+
+    @Column
+    private Long soul2;
+
+    @Column
+    private Long soul3;
+
+    @Column
+    private Long soul4;
+
+    @Column
+    private Long soul5;
 
     @Column
     private Long lastBatch;
 
+
     @Builder
-    public SoulconnectBatchdata(String tacticAndSoul){
-        this.tacticAndSoul = tacticAndSoul;
+    public SoulconnectBatchdata(Long id){
+        this.id = id;
+    }
+
+    public void addSoulId(Long soulId){
+        if (this.soul1 == null){
+            this.soul1 = soulId;
+            return;
+        }
+        if (this.soul2 == null){
+            this.soul2 = soulId;
+        }
+        if (this.soul3 == null){
+            this.soul3 = soulId;
+        }
+        if (this.soul4 == null){
+            this.soul4 = soulId;
+        }
+        if (this.soul5 == null){
+            this.soul5 = soulId;
+        }
     }
 }
