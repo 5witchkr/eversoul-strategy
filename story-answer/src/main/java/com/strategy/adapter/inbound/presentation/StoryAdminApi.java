7f6262dbef220e059de54ccc9e6ad30398f41b3d
5witchkr <hob4419@gmail.com>
2023-02-04 00:46:30 +0900

    refactor: add StoryEnding Port - facade

--- story-answer/src/main/java/com/strategy/adapter/inbound/presentation/StoryAdminApi.java
+++ story-answer/src/main/java/com/strategy/adapter/inbound/presentation/StoryAdminApi.java
@@ -1,7 +1,7 @@
 package com.strategy.adapter.inbound.presentation;
 
-import com.strategy.application.facade.StoryEndingFacade;
-import com.strategy.application.facade.StoryManagementFacade;
+import com.strategy.application.facade.StoryEndingPortFacade;
+import com.strategy.application.facade.StoryManagementPortFacade;
 import com.strategy.application.port.inbound.inputdto.StoryAnswerSaveDto;
 import com.strategy.application.port.inbound.inputdto.StoryEpisodeSaveDto;
 import com.strategy.application.port.inbound.inputdto.StoryQuestionSaveDto;
@@ -21,12 +21,13 @@ public class StoryAdminApi {
     @Value("${devValue}")
     private String value;
 
-    private final StoryEndingFacade storyEndingFacade;
-    private final StoryManagementFacade storyManagementFacade;
+    private final StoryEndingPortFacade storyEndingPortFacade;
 
-    public StoryAdminApi(StoryEndingFacade storyEndingFacade, StoryManagementFacade storyManagementFacade) {
-        this.storyEndingFacade = storyEndingFacade;
-        this.storyManagementFacade = storyManagementFacade;
+    private final StoryManagementPortFacade storyManagementPortFacade;
+
+    public StoryAdminApi(StoryEndingPortFacade storyEndingPortFacade, StoryManagementPortFacade storyManagementPortFacade) {
+        this.storyEndingPortFacade = storyEndingPortFacade;
+        this.storyManagementPortFacade = storyManagementPortFacade;
     }
 
 
@@ -34,7 +35,7 @@ public class StoryAdminApi {
     public ResponseEntity<List<StoryQuestionEndingResponseDto>> recommendTactic(@PathVariable Long soulId,
                                                                                 @RequestParam int episode,
                                                                                 @RequestParam String ending) {
-        return ResponseEntity.ok().body(storyEndingFacade.getSoulEndingByEpisode(soulId,episode,ending));
+        return ResponseEntity.ok().body(storyEndingPortFacade.getSoulEndingByEpisode(soulId,episode,ending));
     }
 
 
@@ -42,24 +43,24 @@ public class StoryAdminApi {
     public void postStorySoul(@RequestParam String devValue,
                               @RequestBody StorySoulSaveDto soulSaveDto){
         if (!devValue.equals(value)) return;
-        storyManagementFacade.postSoul(soulSaveDto);
+        storyManagementPortFacade.postSoul(soulSaveDto);
     }
     @PostMapping("/episode")
     public void postStoryEpisode(@RequestParam String devValue,
                               @RequestBody StoryEpisodeSaveDto storyEpisodeSaveDto){
         if (!devValue.equals(value)) return;
-        storyManagementFacade.postEpisode(storyEpisodeSaveDto);
+        storyManagementPortFacade.postEpisode(storyEpisodeSaveDto);
     }
     @PostMapping("/question")
     public void postStoryQuestion(@RequestParam String devValue,
                               @RequestBody StoryQuestionSaveDto storyQuestionSaveDto){
         if (!devValue.equals(value)) return;
-        storyManagementFacade.postQuestion(storyQuestionSaveDto);
+        storyManagementPortFacade.postQuestion(storyQuestionSaveDto);
     }
     @PostMapping("/answer")
     public void postStoryAnswer(@RequestParam String devValue,
                               @RequestBody StoryAnswerSaveDto storyAnswerSaveDto){
         if (!devValue.equals(value)) return;
-        storyManagementFacade.postAnswer(storyAnswerSaveDto);
+        storyManagementPortFacade.postAnswer(storyAnswerSaveDto);
     }
 }
