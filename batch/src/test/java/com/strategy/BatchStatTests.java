698878830f5af098d965bf696375702d06680cfd
5witchkr <hob4419@gmail.com>
2023-02-03 23:18:24 +0900

    feat: init batchStat test

---
+++ batch/src/test/java/com/strategy/BatchStatTests.java
@@ -0,0 +1,9 @@
+package com.strategy;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchStatTests {
+    public void contextLoads() {}
+}
