7cbc10a3bfd675d0e6316650322a03b1d753b574
5witchkr <hob4419@gmail.com>
2023-02-03 23:18:41 +0900

    feat: init selectNumber arg check test

---
+++ batch/src/main/java/com/strategy/application/validator/StatSelectNumberValidator.java
@@ -0,0 +1,10 @@
+package com.strategy.application.validator;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StatSelectNumberValidator {
+
+    // todo top args number (soul) check
+}
