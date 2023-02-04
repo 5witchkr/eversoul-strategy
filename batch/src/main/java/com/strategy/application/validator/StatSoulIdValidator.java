b2784e88d78d475dae8ef19893f9f181f244c40e
5witchkr <hob4419@gmail.com>
2023-02-04 11:08:33 +0900

    feat: checkSoulIdValidator

--- batch/src/main/java/com/strategy/application/validator/StatSoulIdValidator.java
+++ batch/src/main/java/com/strategy/application/validator/StatSoulIdValidator.java
@@ -1,10 +1,18 @@
 package com.strategy.application.validator;
 
 
+import com.strategy.enummodel.SoulIdEnum;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StatSoulIdValidator {
 
-    //todo soul Id check
+
+    public void checkSoulId(Long id){
+        if (SoulIdEnum.SOUL_ID_START.getValue() <= id
+                && id <= SoulIdEnum.SOUL_ID_END.getValue()) {
+            return;
+        }
+        throw new IllegalArgumentException("유효하지 않은 SoulId");
+    }
 }
