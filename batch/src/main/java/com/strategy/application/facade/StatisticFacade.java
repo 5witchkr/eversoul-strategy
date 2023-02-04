342d1eb3c4ca115dfb70d86c0b6b562f834ea373
5witchkr <hob4419@gmail.com>
2023-02-04 12:05:53 +0900

    feat: port-facade add rating port

--- batch/src/main/java/com/strategy/application/facade/StatisticFacade.java
+++ batch/src/main/java/com/strategy/application/facade/StatisticFacade.java
@@ -5,19 +5,26 @@ import com.strategy.application.port.inbound.portstat.GetPositionStatInboundPort
 import com.strategy.application.port.inbound.portstat.GetSoulSelectStatInboundPort;
 import com.strategy.application.port.inbound.outputdto.PositionStatResponseDto;
 import com.strategy.application.port.inbound.outputdto.SoulSelectResponseDto;
+import com.strategy.application.validator.StatSelectNumberValidator;
+import com.strategy.application.validator.StatSoulIdValidator;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class StatisticFacade implements StatisticPortFacade{
 
     private final GetPositionStatInboundPort getPositionStatInboundPort;
     private final GetSoulSelectStatInboundPort getSoulSelectStatInboundPort;
+    private final StatSelectNumberValidator statSelectNumberValidator;
+    private final StatSoulIdValidator statSoulIdValidator;
 
-    public StatisticFacade(GetPositionStatInboundPort getPositionStatInboundPort, GetSoulSelectStatInboundPort getSoulSelectStatInboundPort) {
+    public StatisticFacade(GetPositionStatInboundPort getPositionStatInboundPort, GetSoulSelectStatInboundPort getSoulSelectStatInboundPort, StatSelectNumberValidator statSelectNumberValidator, StatSoulIdValidator statSoulIdValidator) {
         this.getPositionStatInboundPort = getPositionStatInboundPort;
         this.getSoulSelectStatInboundPort = getSoulSelectStatInboundPort;
+        this.statSelectNumberValidator = statSelectNumberValidator;
+        this.statSoulIdValidator = statSoulIdValidator;
     }
 
 
@@ -28,11 +35,17 @@ public class StatisticFacade implements StatisticPortFacade{
 
     @Override
     public SoulSelectResponseDto getOneSoulSelect(Long soulId) {
+        statSoulIdValidator.checkSoulId(soulId);
         return getSoulSelectStatInboundPort.getOne(soulId);
     }
 
     @Override
-    public List<SoulSelectResponseDto> getTopSoulSelect(int argNumber) {
-        return getSoulSelectStatInboundPort.getTopLate(argNumber);
+    public List<SoulSelectResponseDto> getSoulSelect(int argNumber, String rating) {
+        statSelectNumberValidator.checkSelectNumber(argNumber);
+
+        if (Objects.equals(rating, "bottom")) {
+            return getSoulSelectStatInboundPort.getBottomRating(argNumber);
+        }
+        return getSoulSelectStatInboundPort.getTopRating(argNumber);
     }
 }
