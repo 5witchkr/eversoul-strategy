62a99a0b343a91e20c513e7f90a292de95f516bd
5witchkr <hob4419@gmail.com>
2023-02-06 01:32:50 +0900

    feat: persistence jpaRepository StatSoulConnect

--- batch/src/main/java/com/strategy/adapter/outbound/persistence/StatisticSoulConnectRepository.java
+++ batch/src/main/java/com/strategy/adapter/outbound/persistence/StatisticSoulConnectRepository.java
@@ -2,5 +2,8 @@ package com.strategy.adapter.outbound.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StatisticSoulConnectRepository extends JpaRepository<StatisticSoulconnect, Long> {
+    Optional<StatisticSoulconnect> findByConnectSoulAndConnectedSoul(Long aLong, Long index);
 }
