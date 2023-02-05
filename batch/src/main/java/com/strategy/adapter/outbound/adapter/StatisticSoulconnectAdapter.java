36d905f751513b58d834b392bdfe4b747766e476
5witchkr <hob4419@gmail.com>
2023-02-06 03:07:58 +0900

    feat: adapter statSoulconnect add method

--- batch/src/main/java/com/strategy/adapter/outbound/adapter/StatisticSoulconnectAdapter.java
+++ batch/src/main/java/com/strategy/adapter/outbound/adapter/StatisticSoulconnectAdapter.java
@@ -5,6 +5,7 @@ import com.strategy.adapter.outbound.persistence.StatisticSoulconnect;
 import com.strategy.application.port.outbound.StatisticSoulconnectOutboundPort;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -25,5 +26,20 @@ public class StatisticSoulconnectAdapter implements StatisticSoulconnectOutbound
     @Override
     public StatisticSoulconnect save(StatisticSoulconnect statisticSoulconnect) {
         return statisticSoulConnectRepository.save(statisticSoulconnect);
+    }
+
+    @Override
+    public Optional<List<StatisticSoulconnect>> findTop10ByConnectedSoul(Long connectedSoul) {
+        return statisticSoulConnectRepository.findTop10ByConnectedSoul(connectedSoul);
+    }
+
+    @Override
+    public Optional<List<StatisticSoulconnect>> findTop10ByConnectSoul(Long connectSoul) {
+        return statisticSoulConnectRepository.findTop10ByConnectSoul(connectSoul);
+    }
+
+    @Override
+    public Optional<List<StatisticSoulconnect>> findTop10ByOrderByConnectCountDesc() {
+        return statisticSoulConnectRepository.findTop10ByOrderByConnectCountDesc();
     }
 }
