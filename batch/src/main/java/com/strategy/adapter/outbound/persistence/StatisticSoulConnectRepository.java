7908ce3dece050a00e1e5a2a3ddbcfae67d80824
5witchkr <hob4419@gmail.com>
2023-02-06 03:08:56 +0900

    feat: persistence JpaRepository add top10 queryMethod

--- batch/src/main/java/com/strategy/adapter/outbound/persistence/StatisticSoulConnectRepository.java
+++ batch/src/main/java/com/strategy/adapter/outbound/persistence/StatisticSoulConnectRepository.java
@@ -2,8 +2,15 @@ package com.strategy.adapter.outbound.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StatisticSoulConnectRepository extends JpaRepository<StatisticSoulconnect, Long> {
     Optional<StatisticSoulconnect> findByConnectSoulAndConnectedSoul(Long aLong, Long index);
+
+    Optional<List<StatisticSoulconnect>> findTop10ByConnectSoul(Long connectSoul);
+
+    Optional<List<StatisticSoulconnect>> findTop10ByConnectedSoul(Long connectedSoul);
+
+    Optional<List<StatisticSoulconnect>> findTop10ByOrderByConnectCountDesc();
 }
