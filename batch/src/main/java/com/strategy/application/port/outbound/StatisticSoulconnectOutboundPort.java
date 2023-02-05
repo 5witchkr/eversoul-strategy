99c7d425082e8283f18c2ff6f41d74784bc9e8d1
5witchkr <hob4419@gmail.com>
2023-02-06 03:08:24 +0900

    feat: outbound-port StatSoulConnect use method

--- batch/src/main/java/com/strategy/application/port/outbound/StatisticSoulconnectOutboundPort.java
+++ batch/src/main/java/com/strategy/application/port/outbound/StatisticSoulconnectOutboundPort.java
@@ -2,10 +2,18 @@ package com.strategy.application.port.outbound;
 
 import com.strategy.adapter.outbound.persistence.StatisticSoulconnect;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StatisticSoulconnectOutboundPort {
     Optional<StatisticSoulconnect> findByConnectSoulAndConnectedSoul(Long connectSoul, Long connectedSoul);
 
     StatisticSoulconnect save(StatisticSoulconnect statisticSoulconnect);
+
+    Optional<List<StatisticSoulconnect>> findTop10ByConnectedSoul(Long connectedSoul);
+
+    Optional<List<StatisticSoulconnect>> findTop10ByConnectSoul(Long connectSoul);
+
+    Optional<List<StatisticSoulconnect>> findTop10ByOrderByConnectCountDesc();
+
 }
