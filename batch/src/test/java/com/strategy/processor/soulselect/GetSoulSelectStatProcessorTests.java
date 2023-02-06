2cac69c23e69abbfd7b13df1393fa92acd0f2c2c
5witchkr <hob4419@gmail.com>
2023-02-07 01:17:35 +0900

    feat: test StatisticSoulselect getBottomRating success case

--- batch/src/test/java/com/strategy/processor/soulselect/GetSoulSelectStatProcessorTests.java
+++ batch/src/test/java/com/strategy/processor/soulselect/GetSoulSelectStatProcessorTests.java
@@ -52,12 +52,33 @@ public class GetSoulSelectStatProcessorTests {
         final List<SoulSelectResponseDto> answer = List.of(
                 SoulSelectResponseDto.builder().id(4L).selectCount(5).build(),
                 SoulSelectResponseDto.builder().id(3L).selectCount(4).build(),
-                SoulSelectResponseDto.builder().id(2L).selectCount(3).build()
+                SoulSelectResponseDto.builder().id(1L).selectCount(3).build()
         );
         final int argNumber = 3;
 
         return Stream.of(DynamicTest.dynamicTest("성공케이스: topRating soulselect를 반환한다.", () -> {
             List<SoulSelectResponseDto> result = getSoulSelectStatProcessor.getTopRating(argNumber);
+
+            assertThat(result.size()).isEqualTo(answer.size());
+            assertThat(result.get(0)).isInstanceOf(SoulSelectResponseDto.class);
+            assertThat(result).usingRecursiveFieldByFieldElementComparator().isEqualTo(answer);
+        }));
+    }
+
+    @DisplayName("getBottomRating tests")
+    @TestFactory
+    Stream<DynamicTest> testGetBottomRating() {
+        statisticSoulSelectOutboundPort = new TestStatisticSoulSelectOutboundPort();
+        getSoulSelectStatProcessor = new GetSoulSelectStatProcessor(statisticSoulSelectOutboundPort);
+        final List<SoulSelectResponseDto> answer = List.of(
+                SoulSelectResponseDto.builder().id(2L).selectCount(2).build(),
+                SoulSelectResponseDto.builder().id(1L).selectCount(3).build(),
+                SoulSelectResponseDto.builder().id(3L).selectCount(4).build()
+        );
+        final int argNumber = 3;
+
+        return Stream.of(DynamicTest.dynamicTest("성공케이스: bottomRating soulselect를 반환한다.", () -> {
+            List<SoulSelectResponseDto> result = getSoulSelectStatProcessor.getBottomRating(argNumber);
 
             assertThat(result.size()).isEqualTo(answer.size());
             assertThat(result.get(0)).isInstanceOf(SoulSelectResponseDto.class);
@@ -82,8 +103,8 @@ public class GetSoulSelectStatProcessorTests {
         @Override
         public List<StatisticSoulselect> findAll() {
             return List.of(
-                    StatisticSoulselect.builder().id(1L).selectCount(2).build(),
-                    StatisticSoulselect.builder().id(2L).selectCount(3).build(),
+                    StatisticSoulselect.builder().id(1L).selectCount(3).build(),
+                    StatisticSoulselect.builder().id(2L).selectCount(2).build(),
                     StatisticSoulselect.builder().id(3L).selectCount(4).build(),
                     StatisticSoulselect.builder().id(4L).selectCount(5).build()
                     );
